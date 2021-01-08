eae6e800d431fb163e827166921b00352cf1ea51
asad.mehmood <asad.mehmood@TA-NOC-WASIQ>
2021-01-08 17:36:36 +0500

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const http = require("http");
+const Websocket = require("ws");
+const port = 6969;
+const server = http.createServer({express})
+const wss = new Websocket.Server({server})
+
+wss.on("connection", (ws) => {
+    ws.on("message", (data) => {
+        wss.clients.forEach(function each(client) {
+            if (client !== ws && client.readyState === Websocket.OPEN) {
+                client.send(data);
+            }
+        })
+    })
+})
+
+server.listen(port, () => {
+    console.log("Hurray Port is listening");
+})
